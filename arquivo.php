8812c1d5ac3c3ddcfbccc666a17ca22e51e73745
Ana Clara Da Silva <67916055+aanagomes@users.noreply.github.com>
2022-06-26 11:29:41 -0300

    Update arquivo.php

--- arquivo.php
+++ arquivo.php
@@ -1,23 +1,23 @@
 <?php
  include "conexao.php";
  
- 	//$vnome=$_POST["nome"];
-	//$vdatanas=$_POST["dt_nasc"];
-	//$vsexo=$_POST["sexo"];
-	//$vcpf=$_POST["cpf"];
-	//$vrg=$_POST["rg"];
-	//$vestadocivil=$_POST["estado_civil"];
-	//$vtelefone=$_POST["telefone"];
-	//$vtelefone2=$_POST["telefone_2"];
-	//$vendereco=$_POST["endereco"];
-	//$vnumero=$_POST["numero"];
-	//$vbairro=$_POST["bairro"];
-	//$vpontoref=$_POST["ponto_ref"];
-	//$vcidade=$_POST["cidade"];
-	//$vcep=$_POST["cep"];
-	//$vtemporesi=$_POST["tempo_resi"];
-	//$vtipo=$_POST["tipo"];
-	//$vvalor=$_POST["preco"];
+ 	$vnome=$_POST["nome"];
+	$vdatanas=$_POST["dt_nasc"];
+	$vsexo=$_POST["sexo"];
+	$vcpf=$_POST["cpf"];
+	$vrg=$_POST["rg"];
+	$vestadocivil=$_POST["estado_civil"];
+	$vtelefone=$_POST["telefone"];
+	$vtelefone2=$_POST["telefone_2"];
+	$vendereco=$_POST["endereco"];
+	$vnumero=$_POST["numero"];
+	$vbairro=$_POST["bairro"];
+	$vpontoref=$_POST["ponto_ref"];
+	$vcidade=$_POST["cidade"];
+	$vcep=$_POST["cep"];
+	$vtemporesi=$_POST["tempo_resi"];
+	$vtipo=$_POST["tipo"];
+	$vvalor=$_POST["preco"];
 
 
 
@@ -28,12 +28,11 @@
 	$vsalariod=$_POST["salariod"];
 
 	
-	//$sql1="INSERT INTO dados_pessoais 
-	//		VALUES('$vcpf','$vnome','$vsexo','$vrg','$vdatanas','$vestadocivil','$vendereco','$vnumero',$vbairro','$vpontoref','$vcidade','$vcep','$vtelefone','$vtelefone2','$vtemporesi','$vtipo',$vvalor)";
 	
-	$sql="INSERT INTO dependentes(nome,idade,parentesco,profissao,salario) VALUES ('$vnomed',$vidaded,'$vparentescod','$vprofissaod',$vsalariod)";
+	$sql1="INSERT INTO dados_pessoais VALUES('$vcpf','$vnome','$vsexo','$vrg','$vdatanas','$vestadocivil','$vendereco','$vnumero','$vbairro','$vpontoref','$vcidade','$vcep','$vtelefone','$vtelefone2','$vtemporesi','$vtipo','$vvalor')";
+	$sql2="INSERT INTO dependentes(nome,idade,parentesco,profissao,salario) VALUES ('$vnomed',$vidaded,'$vparentescod','$vprofissaod',$vsalariod)";
 
-	mysqli_query($conexao,$sql);
+	mysqli_query($conexao,$sql1,$sql2);
 	$linhas=mysqli_affected_rows($conexao);
 
 	if ($linhas == 1){
@@ -45,17 +44,3 @@
 	mysqli_close($conexao);
 
 ?>
-
-
-
-
-
-
-<!--
-<
-	$vnome=$_POST["nome"];
-	$vdatanas=$_POST["data_nasc"];
-	
-	echo "Nome: ".$vnome."<br/>Data de Nascimento: ".$vdatanas."<br/>"
-?>
--->
